7aff5e0ebbf90412270c7499277dad567d2674db
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-17 10:37:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function firstWord(s) {
   // your code here
 	s = s.trim();
 
-	const spaceIndex = str.indexOf(' ');
+	const spaceIndex = s.indexOf(' ');
 	 if (spaceIndex === -1) {
-    return str;
+    return s;
   }
-	return str.substring(0, spaceIndex);
+	return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
