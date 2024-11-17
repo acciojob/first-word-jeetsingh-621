286d0ae39a69237cd17e4b5540dd3d1994ecb0c4
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-17 10:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+
+	const spaceIndex = str.indexOf(' ');
+	 if (spaceIndex === -1) {
+    return str;
+  }
+	return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
